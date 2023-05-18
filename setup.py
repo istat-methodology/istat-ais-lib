96cd01c63322128f7a808ad714a6e75693141f25
francescoamato <francamato@gmail.com>
2023-05-18 11:17:58 +0200

    fix python package

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='istat_ais',
+    name='istatais',
     version='0.0.1',
     author=['Francesco Amato', 'ISTAT'],
     author_email='framato@istat.it',
     description='Ais ISTAT wrapper functions',
-    packages = ['istat_ais']
+    packages = ['istatais']
 )
 
 
