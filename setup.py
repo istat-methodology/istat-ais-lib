49d73ba67a9f54db175c0bfe760c7af1cc6705da
Francesco Amato <49312682+francescoamato@users.noreply.github.com>
2023-05-18 11:13:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='istat-ais',
+    name='istat_ais',
     version='0.0.1',
     author=['Francesco Amato', 'ISTAT'],
     author_email='framato@istat.it',
     description='Ais ISTAT wrapper functions',
-    packages = ['istat-ais']
+    packages = ['istat_ais']
 )
 
 
